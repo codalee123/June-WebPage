48b26b1153394a73e21c0c4bfed6812e52791b7c
isinguzoro <61542175+isinguzoro@users.noreply.github.com>
2025-12-17 23:24:27 +0100

    Update home.jsx

--- src/Home/home.jsx
+++ src/Home/home.jsx
@@ -3,7 +3,7 @@ import Hero from '../pages/Hero';
 import WhyJune from '../pages/WhyJune';
 import Built from '../pages/Built';
 import Explore from '../pages/Explore';
-//import Testimony from '../pages/Testimony';
+import Testimony from '../pages/Testimony';
 
 // MainHome Component
 // const MainHome = () => {
@@ -31,7 +31,8 @@ const MainHome = () => {
         <WhyJune/>
         <Built/>
       </div>
-           <Explore/>
+      <Testimony/>
+      <Explore/>
     </>
   )
 }
